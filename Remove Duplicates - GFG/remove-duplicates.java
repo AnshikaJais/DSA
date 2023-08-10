b2d2ae6183657a182fea67fb9473564f2514d0f3
Anshika Jaiswal <jaiswal.anshika2002@gmail.com>
2023-08-10 11:44:59 +0530

    Added solution - LeetHub

---
+++ Remove Duplicates - GFG/remove-duplicates.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeDups(String S) {
+        // code here
+        String str = "";
+        for(char ch : S.toCharArray()){
+            if(str.indexOf(ch) == -1) str += ch;
+        }
+        return str;
+    }
+}

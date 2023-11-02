7a48297ef5b86cdbc741bf6d64c4df0b5b761052
Anshika Jaiswal <jaiswal.anshika2002@gmail.com>
2023-11-02 20:16:53 +0530

    Added solution - LeetHub

---
+++ Possible paths between 2 vertices - GFG/possible-paths-between-2-vertices.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            String[] S1 = br.readLine().trim().split(" ");
+            int source = Integer.parseInt(S1[0]);
+            int destination = Integer.parseInt(S1[1]);
+            Solution obj = new Solution();
+            System.out.println(obj.countPaths(V, adj, source, destination));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count paths between two vertices in a directed graph.
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination) {
+        // Code here
+        
+        int[] res = new int[1];
+        res[0] = 0;
+        
+        countPaths(adj, source, destination, res);
+        
+        return res[0];
+    }
+    
+    public void countPaths(ArrayList<ArrayList<Integer>> adj, int curr, int destination, int[] res){
+        
+        if(curr == destination){
+            res[0]++;
+            return;
+        }
+        
+        for(Integer it : adj.get(curr)){
+            countPaths(adj, it, destination, res);
+        }
+    }
+}
